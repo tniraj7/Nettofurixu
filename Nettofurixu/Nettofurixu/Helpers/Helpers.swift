d2b0dd17a475472360bfcffd5d0b741045792d58
Niraj Tiwari <tniraj7@gmail.com>
2021-01-01 23:29:37 +0900

    update model and sample data

--- Nettofurixu/Nettofurixu/Helpers/Helpers.swift
+++ Nettofurixu/Nettofurixu/Helpers/Helpers.swift
@@ -1,18 +1,87 @@
 import Foundation
 import SwiftUI
 
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 1)
+let exampleMovie1 = Movie(
+    id: UUID().uuidString,
+    name: "DARK",
+    thumbnailURL: URL(string: "https://picsum.photos/200/300")!,
+    categories: ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"],
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 1,
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hoffman, Oliver Masucci, Jordis Triebel")
 
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/300/")!, categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"], year: 2020, rating: "TV-MA",  numberOfSeasons: 2, promotionHeadline: "Best Rated Show")
+let exampleMovie2 = Movie(
+    id: UUID().uuidString,
+    name: "Travelers",
+    thumbnailURL: URL(string: "https://picsum.photos/200/300/")!,
+    categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"],
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 2,
+    promotionHeadline: "Best Rated Show",
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hoffman, Oliver Masucci, Jordis Triebel")
 
-let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/301/")!, categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"], year: 2020, rating: "TV-MA",  numberOfSeasons: 3)
+let exampleMovie3 = Movie(
+    id: UUID().uuidString,
+    name: "Community",
+    thumbnailURL: URL(string: "https://picsum.photos/200/301/")!,
+    categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"],
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 3,
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hoffman, Oliver Masucci, Jordis Triebel")
 
 
-let exampleMovie4 =  Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/302/")!, categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"], year: 2020, rating: "TV-MA",  numberOfSeasons: 4, promotionHeadline: "New episodes coming soon")
+let exampleMovie4 =  Movie(
+    id: UUID().uuidString,
+    name: "Alone",
+    thumbnailURL: URL(string: "https://picsum.photos/200/302/")!,
+    categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"],
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 4,
+    promotionHeadline: "New episodes coming soon",
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hoffman, Oliver Masucci, Jordis Triebel")
 
-let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/303/")!, categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"], year: 2020, rating: "TV-MA",  numberOfSeasons: 5)
+let exampleMovie5 = Movie(
+    id: UUID().uuidString,
+    name: "Hannibal",
+    thumbnailURL: URL(string: "https://picsum.photos/200/303/")!,
+    categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"],
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 5,
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hoffman, Oliver Masucci, Jordis Triebel")
 
-let exampleMovie6 =     Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/304/")!, categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"], year: 2020, rating: "TV-MA",  numberOfSeasons: 6, promotionHeadline: "Watch Season 6 Now")
+let exampleMovie6 =     Movie(
+    id: UUID().uuidString,
+    name: "After Life",
+    thumbnailURL: URL(string: "https://picsum.photos/200/304/")!,
+    categories:  ["Dystopian", "Exciting", "Suspense", "Sci-Fi TV"],
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    year: 2020, rating: "TV-MA",
+    numberOfSeasons: 6,
+    promotionHeadline: "Watch Season 6 Now",
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hoffman, Oliver Masucci, Jordis Triebel")
+
+let exampleEpisodeInfo1 = CurrentEpisodeInfo(
+    episodeName: "Beginnings and Endings",
+    description: "Six months after the disappearances, the police from a task force. In 2052, Jonas learns that most of Winden persihed in an Apocalyptic event.",
+    season: 2,
+    episode: 1
+)
 
 let exampleMovies: [Movie] = [
     exampleMovie1,
--- Nettofurixu/Nettofurixu/Models/Movie.swift
+++ Nettofurixu/Nettofurixu/Models/Movie.swift
@@ -6,6 +6,10 @@ struct Movie: Identifiable {
     var thumbnailURL: URL
     var categories: [String]
     
+    // personalization
+    var currentEpisode: CurrentEpisodeInfo?
+    var defaultEpisodeInfo: CurrentEpisodeInfo
+    
     var episodes: [Episode]?
     var year: Int
     var rating: String
@@ -21,4 +25,33 @@ struct Movie: Identifiable {
         }
         return ""
     }
+    
+    // creators
+    
+    var creators: String
+    var cast: String
+    
+    var episodeInfoDisplay: String {
+        if let current  = currentEpisode {
+            return "S\(current.season):E\(current.episode) \(current.episodeName)"
+        } else {
+            return "S\(defaultEpisodeInfo.season):E\(defaultEpisodeInfo.episode) \(defaultEpisodeInfo.episodeName)"
+        }
+    }
+    
+    var episodeDescriptionDisplay: String {
+        if let current = currentEpisode {
+            return current.description
+        } else {
+            return defaultEpisodeInfo.description
+        }
+    }
+}
+
+struct CurrentEpisodeInfo: Hashable, Equatable {
+    var episodeName: String
+    var description: String
+    var season: Int
+    var episode: Int
+    
 }
