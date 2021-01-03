9d464f2ab308eeccbed9db0914ca435837311710
Niraj Tiwari <tniraj7@gmail.com>
2021-01-03 22:28:12 +0900

    switch the tabs based on state variable

--- Nettofurixu/Nettofurixu/Views/CustomTabSwitcherView.swift
+++ Nettofurixu/Nettofurixu/Views/CustomTabSwitcherView.swift
@@ -2,14 +2,14 @@ import SwiftUI
 
 enum CustomTab: String {
     case episodes = "EPISODES"
-    case trailers = "TRAILERS"
+    case trailers = "TRAILERS & MORE"
     case more = "MORE LIKE THIS"
 }
 
 struct CustomTabSwitcherView: View {
     
     var tabs: [CustomTab]
-    
+    @State private var currentTab: CustomTab = .episodes
     
     private func widthForTab(_ tab: CustomTab) -> CGFloat {
         let string = tab.rawValue
@@ -19,26 +19,38 @@ struct CustomTabSwitcherView: View {
     var body: some View {
         VStack {
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(spacing: 20) {
                     ForEach(tabs, id: \.self ) { tab in
                         VStack {
-                            
                             Rectangle()
                                 .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor( tab == currentTab ? Color.red : Color.clear)
+                            
                             Button(action: {
-                                
+                                currentTab = tab
                             }, label: {
                                 Text(tab.rawValue)
                                     .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor( tab == currentTab ? Color.white : Color.gray)
                             })
                             .buttonStyle(PlainButtonStyle())
+                            .frame(width: widthForTab(tab), height: 30)
                            
                         }
                     }
                 }
             }
             
-            Text("Selected VIew")
+            switch currentTab {
+            case .episodes:
+                Text("EPISODES")
+                
+            case .trailers:
+                Text("TRAILERS & MORE")
+                
+            case .more:
+                Text("MORE")
+            }
         }
         .foregroundColor(.white)
     }
