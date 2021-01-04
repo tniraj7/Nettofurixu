990f8a146e6f63985b156a58a2ebfb5fbdcd283e
Niraj Tiwari <tniraj7@gmail.com>
2021-01-04 21:01:20 +0900

    add  MoreLikeThisView

---
+++ Nettofurixu/Nettofurixu/Views/MoreLikeThisView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct MoreLikeThisView: View {
+    
+    var movies: [Movie]
+    var columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(0..<movies.count) { index in
+                    StandardHomeMovieView(movie: movies[index])
+                }
+            }
+        }
+    }
+}
+
+struct MoreLikeThisView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreLikeThisView(movies: exampleMovies)
+    }
+}
