8a7a64a6ea2ba3b6f5ced55579d79c1698c18638
Niraj Tiwari <tniraj7@gmail.com>
2021-01-05 18:45:04 +0900

    modify model and add mock data

--- Nettofurixu/Nettofurixu/Helpers/Helpers.swift
+++ Nettofurixu/Nettofurixu/Helpers/Helpers.swift
@@ -1,6 +1,36 @@
 import Foundation
 import SwiftUI
 
+
+let exampleVideoURL = URL(string: "https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4")!
+
+let episode1 = Episode(
+    name: "Beginnings and Endings",
+    season: 1,
+    episodeNumber: 1,
+    thumbnailImageURLString:  "https://picsum.photos/300/102",
+    description: "Six months after the disappearances, the police from a task force. In 2052, Jonas learns that most of Winden persihed in an Apocalyptic event.",
+    length: 53,
+    videoURL: exampleVideoURL)
+
+let episode2 = Episode(
+    name: "Dark Matter",
+    season: 1,
+    episodeNumber: 2,
+    thumbnailImageURLString:  "https://picsum.photos/300/102",
+    description: "Clausen and Charlette interview Regina. The stranger takes Hannah to 1987, where Claudia has an unnerving encounter and Egon visits an old nemesis.",
+    length: 54,
+    videoURL: exampleVideoURL)
+
+let episode3 = Episode(
+    name: "Ghosts",
+    season: 1,
+    episodeNumber: 3,
+    thumbnailImageURLString:  "https://picsum.photos/300/102",
+    description: "In 1954, a missing Helge returns, but he'll only speak to Noah. In 1987, Claudia brings the time machine to Tannhaus, and Egon questions Elrich again.",
+    length: 56,
+    videoURL: exampleVideoURL)
+
 let exampleMovie1 = Movie(
     id: UUID().uuidString,
     name: "DARK",
--- Nettofurixu/Nettofurixu/Models/Episode.swift
+++ Nettofurixu/Nettofurixu/Models/Episode.swift
@@ -4,10 +4,13 @@ struct Episode: Identifiable {
     var id =  UUID().uuidString
     var name: String
     var season: Int
+    var episodeNumber: Int
     var thumbnailImageURLString: String
     var description: String
     var length: Int
      
+    var videoURL: URL
+    
     var thumbnail: URL {
         return URL(string: thumbnailImageURLString)!
     }
