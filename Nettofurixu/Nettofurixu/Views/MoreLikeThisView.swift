16adf76e2dbb1a629fded33c663bcbc22fffe97e
Niraj Tiwari <tniraj7@gmail.com>
2021-01-07 20:12:12 +0900

    remove extra scroll view for MoreLikethis view

--- Nettofurixu/Nettofurixu/Views/MoreLikeThisView.swift
+++ Nettofurixu/Nettofurixu/Views/MoreLikeThisView.swift
@@ -10,11 +10,9 @@ struct MoreLikeThisView: View {
     ]
     
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            LazyVGrid(columns: columns) {
-                ForEach(0..<movies.count) { index in
-                    StandardHomeMovieView(movie: movies[index])
-                }
+        LazyVGrid(columns: columns) {
+            ForEach(0..<movies.count) { index in
+                StandardHomeMovieView(movie: movies[index])
             }
         }
     }
